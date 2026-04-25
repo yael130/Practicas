c9282b6b54e57da97f1ebf79035bef347263a618
yael130 <yoaschavez1@gmail.com>
2026-04-25 11:35:16 -0600

    Acabo de crear el proyecto de angular

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+  title = 'Hola mundo angualar';
+}
+AbortSignal
